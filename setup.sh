78617166425cbcceeddf7f59730fe6ddba6e50e6
BobAnkh <bobankhshen@gmail.com>
2021-06-23 11:15:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ sudo apt-get install zsh -y
 
 # install zsh extensions
 mk_folder .zsh
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
+git clone --depth=1 http://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
+git clone --depth=1 http://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone --depth=1 http://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
 
 # change shell
 chsh -s /usr/bin/zsh
