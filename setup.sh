ff3f556374fd01266a9fff07c201f6e4834be9f5
BobAnkh <bobankhshen@gmail.com>
2021-06-23 11:08:39 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/sh
+
+mk_folder() {
+    if [ ! -d "$1" ]; then
+        mkdir -p "$1"
+        echo "Create folder $1"
+    fi
+}
+
+
 # install zsh
 sudo apt-get install zsh -y
 
 # install zsh extensions
-mkdir .zsh
+mk_folder .zsh
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
