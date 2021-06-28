0e74d29e2b9b266bfde43ad4cb6a8c71c29c8a46
BobAnkh <bobankhshen@gmail.com>
2021-06-28 19:56:56 +0800

    Update setup.sh

--- modern-unix/setup.sh
+++ modern-unix/setup.sh
@@ -5,6 +5,16 @@ sudo apt install bat -y
 wget https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_amd64.deb
 sudo dpkg -i lsd_0.20.1_amd64.deb
 
+# install rust
+if command -v rustc >/dev/null 2>&1; then
+    echo 'Exists rustc...skip intstall!'
+else
+    echo 'No exists rustc...install rust!'
+    curl https://sh.rustup.rs -sSf | sh -s
+    # shellcheck disable=SC1091
+    source "$HOME"/.cargo/env
+fi
+
 cargo install du-dust
 
 cargo install procs
