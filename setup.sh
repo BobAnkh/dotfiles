23c6a4345707617464a4830a1b39376301539e56
BobAnkh <bobankhshen@gmail.com>
2021-06-30 16:18:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlev
 chsh -s /usr/bin/zsh
 
 # use already-written configurations
-wget https://raw.githubusercontent.com/BobAnkh/dotfiles/main/zsh/.zshrc
-wget https://raw.githubusercontent.com/BobAnkh/dotfiles/main/zsh/.p10k.zsh
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/BobAnkh/dotfiles/main/zsh/.zshrc
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/BobAnkh/dotfiles/main/zsh/.p10k.zsh
 
 echo "Please logout and login again!"
