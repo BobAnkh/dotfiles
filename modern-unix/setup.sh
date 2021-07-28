74a4faa81588573515b93552a8610899816ba6db
BobAnkh <bobankhshen@gmail.com>
2021-07-28 09:32:45 +0800

    Update setup.sh

--- modern-unix/setup.sh
+++ modern-unix/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # sudo apt install bat -y
-sudo apt install wget -y
+sudo apt install wget build-essential -y
 wget  --no-check-certificate --content-disposition https://github.com/sharkdp/bat/releases/download/v0.18.1/bat_0.18.1_amd64.deb
 sudo dpkg -i bat_0.18.1_amd64.deb
 
