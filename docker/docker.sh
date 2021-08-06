317bddb7d3d1b58c9859bcab16ea3afc5dd47c07
BobAnkh <bobankhshen@gmail.com>
2021-08-06 17:05:45 +0800

    Update docker.sh

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env bash
-
-# From https://github.com/evertramos/easy-server/blob/master/install/docker/docker.sh
+#!/bin/bash
+###
+ # @Author       : BobAnkh
+ # @Github       : https://github.com/BobAnkh
+ # @Date         : 2021-08-06 17:04:05
+ # @LastEditTime : 2021-08-06 17:05:11
+ # @Description  : 
+### 
 
 #---------------------------------------------------------------------------------
 # Installing Docker
@@ -25,7 +30,7 @@ sudo usermod -aG docker $USER
 COMPOSE_VERSION=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
 # Download the latest release of docker
-sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-"$(uname -s)"-"$(uname -m)" -o /usr/local/bin/docker-compose
 
 # Apply permissions
 sudo chmod +x /usr/local/bin/docker-compose
