85e9a281d06b50dcbb06c26713a24cad8383a7f6
BobAnkh <bobankhshen@gmail.com>
2021-09-24 00:35:36 +0800

    chore(deps): update install dependency

--- modern-unix/setup.sh
+++ modern-unix/setup.sh
@@ -2,7 +2,7 @@
 # sudo apt install bat -y
 sudo apt install wget build-essential -y
 wget  --no-check-certificate --content-disposition https://github.com/sharkdp/bat/releases/download/v0.18.3/bat_0.18.3_amd64.deb
-sudo dpkg -i bat_0.18.1_amd64.deb
+sudo dpkg -i bat_0.18.3_amd64.deb
 
 # cargo install lsd
 wget  --no-check-certificate --content-disposition https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd_0.20.1_amd64.deb
