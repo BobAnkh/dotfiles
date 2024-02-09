a05a8fd1c0eed3b8b3a79afbe91fb262454a7755
BobAnkh <bobankhshen@gmail.com>
2024-02-09 14:02:22 +0000

    Add nvim configuration

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "hardhackerlabs/theme-vim",
+    name = "hardhacker",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.hardhacker_hide_tilde = 1
+      vim.g.hardhacker_keyword_italic = 1
+      -- custom highlights
+      vim.g.hardhacker_custom_highlights = {
+        [[hi NonText guifg=#abb2bf]],
+      }
+      vim.cmd("colorscheme hardhacker")
+    end,
+  },
+}
