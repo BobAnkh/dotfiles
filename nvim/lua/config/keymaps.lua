927d6ad1d08d3b18d5e51ab5bf71e7a0089ce5f4
BobAnkh <bobankhshen@gmail.com>
2024-10-12 08:30:38 +0000

    Add some keymaps to nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Make Y behave like C or D
+vim.keymap.set("n", "Y", "y$")
+
+-- Select allvim.keymap.set("n", "==", "gg<S-v>G")
 vim.keymap.set("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+-- Copy text to " register
+vim.keymap.set("n", "<leader>y", '"+y', { desc = 'Yank into " register' })
+vim.keymap.set("v", "<leader>y", '"+y', { desc = 'Yank into " register' })
+vim.keymap.set("n", "<leader>Y", '"+Y', { desc = 'Yank into " register' })
+
+-- Delete text to " register
+vim.keymap.set("n", "<leader>d", '"_d', { desc = 'Delete into " register' })
+vim.keymap.set("v", "<leader>d", '"_d', { desc = 'Delete into " register' })
 vim.keymap.set("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+
+-- Move block
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Block Down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Block Up" })
+
+-- Move with arrow keys
 vim.keymap.set("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
 vim.keymap.set("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 vim.keymap.set("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
