4fc325be1ff6e86689d3db7944c0dc1f9f985682
BobAnkh <bobankhshen@gmail.com>
2024-12-26 13:51:15 +0000

    Adjust nvim plugins

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,6 @@
+return {
+  "saghen/blink.cmp",
+  opts = function(_, opts)
+    opts.keymap.preset = "super-tab"
+  end,
+}
--- nvim/lua/plugins/smear_cursor.lua
+++
@@ -1,7 +0,0 @@
-if true then
-  return {}
-end
--- return {
---   "sphamba/smear-cursor.nvim",
---   opts = {},
--- }
