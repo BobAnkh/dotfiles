ca124f2d27f748047594773099b24fe83cca504f
BobAnkh <bobankhshen@gmail.com>
2025-01-14 08:17:15 +0000

    Display name of current file and inlay hints

---
+++ nvim/lua/plugins/incline.lua
@@ -0,0 +1,21 @@
+return {
+  "b0o/incline.nvim",
+  event = "BufReadPre",
+  enabled = true,
+  config = function()
+    require("incline").setup({
+      highlight = {
+        groups = {
+          InclineNormal = { guibg = "#46415A", guifg = "#FC56B1" },
+          InclineNormalNC = { guifg = "#9A4D76", guibg = "#3C374D" },
+        },
+      },
+      window = { margin = { vertical = 0, horizontal = 1 } },
+      render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+        return { { icon, guifg = color }, { " " }, { filename } }
+      end,
+    })
+  end,
+}
--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -3,4 +3,46 @@ return {
   opts = function(_, opts)
     opts.server.default_settings["rust-analyzer"]["cargo"]["allFeatures"] = nil
   end,
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        settings = {
+          ["rust-analyzer"] = {
+            inlayHints = {
+              bindingModeHints = {
+                enable = false,
+              },
+              chainingHints = {
+                enable = true,
+              },
+              closingBraceHints = {
+                enable = true,
+                minLines = 25,
+              },
+              closureReturnTypeHints = {
+                enable = "never",
+              },
+              lifetimeElisionHints = {
+                enable = "never",
+                useParameterNames = false,
+              },
+              maxLength = 25,
+              parameterHints = {
+                enable = true,
+              },
+              reborrowHints = {
+                enable = "never",
+              },
+              renderColons = true,
+              typeHints = {
+                enable = true,
+                hideClosureInitialization = false,
+                hideNamedConstructor = false,
+              },
+            },
+          },
+        },
+      },
+    }
+  end,
 }
