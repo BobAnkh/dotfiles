a87fea623837d1be61721062406bef33573d0740
BobAnkh <bobankhshen@gmail.com>
2025-02-06 06:48:38 +0000

    Change formatter of git line blame in gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,11 @@
-return { "lewis6991/gitsigns.nvim", opts = {
-  current_line_blame = true,
-} }
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 250,
+    },
+    current_line_blame_formatter = "     <author>, <author_time:%R> • <summary>",
+    current_line_blame_formatter_nc = "     You, <author_time:%R> • Uncommitted changes",
+  },
+}
