bbe642bed6325ac48b2a513607d09fc4fe3b8490
BobAnkh <bobankhshen@gmail.com>
2025-02-07 13:36:57 +0000

    Improve display of incline in nvim

--- nvim/lua/plugins/incline.lua
+++ nvim/lua/plugins/incline.lua
@@ -13,6 +13,9 @@ return {
       window = { margin = { vertical = 0, horizontal = 1 } },
       render = function(props)
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        if filename == "" then
+          filename = "[No Name]"
+        end
         local icon, color = require("nvim-web-devicons").get_icon_color(filename)
         return { { icon, guifg = color }, { " " }, { filename } }
       end,
