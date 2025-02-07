4cc8bcf2848684c5ebab8f14ee793396fc771af3
BobAnkh <bobankhshen@gmail.com>
2025-02-07 12:55:44 +0000

    Support transparent background for nvim ui

--- nvim/lua/plugins/edgy.lua
+++ nvim/lua/plugins/edgy.lua
@@ -1,28 +1,43 @@
 return {
-  "folke/edgy.nvim",
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    local left = opts["left"]
-    -- function dump(o)
-    --   if type(o) == "table" then
-    --     local s = "{ "
-    --     for k, v in pairs(o) do
-    --       if type(k) ~= "number" then
-    --         k = '"' .. k .. '"'
-    --       end
-    --       s = s .. "[" .. k .. "] = " .. dump(v) .. ","
-    --     end
-    --     return s .. "} "
-    --   else
-    --     return tostring(o)
-    --   end
-    -- end
-    for _, v in pairs(left) do
-      if v["ft"] == "neo-tree" then
-        -- delete first 6 characters
-        v["title"] = v["title"]:sub(9)
-        -- print(dump(v["title"]))
+  {
+    "folke/edgy.nvim",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local left = opts["left"]
+      -- function dump(o)
+      --   if type(o) == "table" then
+      --     local s = "{ "
+      --     for k, v in pairs(o) do
+      --       if type(k) ~= "number" then
+      --         k = '"' .. k .. '"'
+      --       end
+      --       s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+      --     end
+      --     return s .. "} "
+      --   else
+      --     return tostring(o)
+      --   end
+      -- end
+      for _, v in pairs(left) do
+        if v["ft"] == "neo-tree" then
+          -- delete first 6 characters
+          v["title"] = v["title"]:sub(9)
+          -- print(dump(v["title"]))
+        end
       end
-    end
-  end,
+      table.insert(opts, { wo = { winhighlight = "" } })
+    end,
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    opts = {
+      extra_groups = {
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+      },
+    },
+    keys = {
+      { "<leader>ut", "<cmd>TransparentToggle<cr>", desc = "Toggle Transparency" },
+    },
+  },
 }
