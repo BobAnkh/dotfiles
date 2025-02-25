6c145b792e7687ee213882a742076967c7fbf732
BobAnkh <bobankhshen@gmail.com>
2025-02-25 06:35:46 +0000

    Change diff view of snacks.picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -20,6 +20,12 @@ return {
   },
   opts = {
     picker = {
+      previewers = {
+        diff = {
+          builtin = false, -- use Neovim for previewing diffs (true) or use an external tool (false)
+          cmd = { "delta" }, -- example to show a diff with delta
+        },
+      },
       sources = {
         explorer = {
           -- show hidden files like .env
