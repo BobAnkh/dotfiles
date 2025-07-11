07278ebe8a9fb0bbbe005c440bdfc113c3d474b1
BobAnkh <bobankhshen@gmail.com>
2025-07-11 06:35:50 +0000

    Fix blink in lazyvim temporarily

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,7 +1,26 @@
+-- return {
+--   "saghen/blink.cmp",
+--   opts = function(_, opts)
+--     -- TODO: wair for this PR to be merged to set: https://github.com/LazyVim/LazyVim/pull/6183
+--     opts.keymap.preset = "super-tab"
+--     opts.completion.ghost_text.show_with_menu = false
+--   end,
+-- }
 return {
   "saghen/blink.cmp",
-  opts = function(_, opts)
-    opts.keymap.preset = "super-tab"
-    opts.completion.ghost_text.show_with_menu = false
-  end,
+  opts = {
+    keymap = {
+      preset = "super-tab",
+      ["<Tab>"] = {
+        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+        "fallback",
+      },
+    },
+    completion = {
+      ghost_text = {
+        show_with_menu = false,
+      },
+    },
+  },
 }
