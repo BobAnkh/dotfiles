e07c13e35e33df8af7a0b913f56601488860b7c3
BobAnkh <bobankhshen@gmail.com>
2025-09-19 07:06:10 +0000

    Update neovim to newest version

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,21 +1,8 @@
--- return {
---   "saghen/blink.cmp",
---   opts = function(_, opts)
---     -- TODO: wair for this PR to be merged to set: https://github.com/LazyVim/LazyVim/pull/6183
---     opts.keymap.preset = "super-tab"
---     opts.completion.ghost_text.show_with_menu = false
---   end,
--- }
 return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
       preset = "super-tab",
-      ["<Tab>"] = {
-        require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
-        require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
-        "fallback",
-      },
     },
     completion = {
       ghost_text = {
