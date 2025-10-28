a9f3eea961903993c2b398680ed9bf63b05deba0
BobAnkh <bobankhshen@gmail.com>
2025-10-28 02:45:14 +0000

    Remove git-conflict plugin as it seems not maintained and not work

--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -1,26 +1,27 @@
-return {
-  "akinsho/git-conflict.nvim",
-  version = "*",
-  opts = {
-    default_mappings = {
-      ours = "<leader>go",
-      theirs = "<leader>gt",
-      none = "<leader>gn",
-      both = "<leader>gb",
-      next = "]x",
-      prev = "[x",
-    },
-  },
-  keys = {
-    {
-      "<leader>gx",
-      "<cmd>GitConflictListQf<cr>",
-      desc = "List Conflicts",
-    },
-    {
-      "<leader>gr",
-      "<cmd>GitConflictRefresh<cr>",
-      desc = "Refresh Conflicts",
-    },
-  },
-}
+return {}
+-- return {
+--   "akinsho/git-conflict.nvim",
+--   version = "*",
+--   opts = {
+--     default_mappings = {
+--       ours = "<leader>go",
+--       theirs = "<leader>gt",
+--       none = "<leader>gn",
+--       both = "<leader>gb",
+--       next = "]x",
+--       prev = "[x",
+--     },
+--   },
+--   keys = {
+--     {
+--       "<leader>gx",
+--       "<cmd>GitConflictListQf<cr>",
+--       desc = "List Conflicts",
+--     },
+--     {
+--       "<leader>gr",
+--       "<cmd>GitConflictRefresh<cr>",
+--       desc = "Refresh Conflicts",
+--     },
+--   },
+-- }
