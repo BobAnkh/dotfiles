887e6669fc0d4f648a3f3c7a8b9562e77c9968e1
BobAnkh <bobankhshen@gmail.com>
2025-12-16 15:28:26 +0000

    Set uv.nvim to conditionally enabled

--- nvim/lua/plugins/uv.lua
+++ nvim/lua/plugins/uv.lua
@@ -1,5 +1,12 @@
 return {
   "benomahony/uv.nvim",
+  ft = { "python" },
+  dependencies = {
+    "folke/snacks.nvim",
+  },
+  opts = {
+    picker_integration = true,
+  },
   config = function()
     require("uv").setup()
   end,
